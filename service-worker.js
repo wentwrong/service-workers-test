29d2826129a5761dbfde417d9b68351a6fccc804
Pavel <wabloxa@yandex.ru>
2020-06-26 17:28:50 +0300

    Remove reload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,6 @@
 self.addEventListener('fetch', event => {
+    console.log(event);
+    console.log(self);
     event.respondWith(
         new Response(`self.location.origin is ${self.location.origin}`, { headers: { 'Content-Type': 'text/html' } })
     );
