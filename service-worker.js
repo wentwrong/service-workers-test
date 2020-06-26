2d3d00ce224cb72105671a5b0178486fe0f3a4e5
Pavel <wabloxa@yandex.ru>
2020-06-26 16:52:45 +0300

    Fix static page

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,4 @@
 self.addEventListener('fetch', event => {
-    console.log(event);
-    
     event.respondWith(
         new Response(`self.location.origin is ${self.location.origin}`, { headers: { 'Content-Type': 'text/html' } })
     );
